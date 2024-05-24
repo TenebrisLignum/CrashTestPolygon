16abaf5e811371d3555893373ffb379e4214a527
Artem Hermaniuk <artemgerman1706@gmail.com>
2024-05-24 21:13:46 +0300

    Improved hero-section

--- crashtestpolygon.client/src/app/components/landing/hero-section/hero-section.component.ts
+++ crashtestpolygon.client/src/app/components/landing/hero-section/hero-section.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { faEnvelope, faLocationDot, faSuitcase, faLink, faDownload } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
-  selector: 'app-hero-section',
-  templateUrl: './hero-section.component.html',
-  styleUrl: './hero-section.component.scss'
+	selector: 'app-hero-section',
+	templateUrl: './hero-section.component.html',
+	styleUrl: './hero-section.component.scss'
 })
 export class HeroSectionComponent {
-
+	faEnvelope = faEnvelope;
+	faLocationDot = faLocationDot;
+	faSuitcase = faSuitcase;
+	faLink = faLink;
+	faDownload = faDownload;
 }
--- crashtestpolygon.client/src/app/components/landing/landing.module.ts
+++ crashtestpolygon.client/src/app/components/landing/landing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LandingComponent } from './landing.component';
 import { HeroSectionComponent } from './hero-section/hero-section.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
 	declarations: [
@@ -9,7 +10,8 @@ import { HeroSectionComponent } from './hero-section/hero-section.component';
 		HeroSectionComponent
 	],
 	imports: [
-		CommonModule
+		CommonModule,
+		FontAwesomeModule
 	],
 	exports: [
 		LandingComponent
