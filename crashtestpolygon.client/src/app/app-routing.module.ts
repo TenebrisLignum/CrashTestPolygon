c5b0d1dfd472943f7561464e4fc05aa30eacef72
Artem Hermaniuk <artemgerman1706@gmail.com>
2024-06-06 12:52:29 +0300

    Added guards

--- crashtestpolygon.client/src/app/app-routing.module.ts
+++ crashtestpolygon.client/src/app/app-routing.module.ts
@@ -3,11 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { LandingComponent } from './components/landing/landing.component';
 import { BlogsComponent } from './components/blogs/blogs.component';
 import { LoginComponent } from './components/login/login.component';
+import { LoginGuard } from './core/guards/login.guard';
 
 const routes: Routes = [
     { path: '', component: LandingComponent },
     { path: 'blogs', component: BlogsComponent },
-    { path: 'login', component: LoginComponent }
+    { path: 'login', component: LoginComponent, canActivate: [LoginGuard] }
 ];
 
 @NgModule({
