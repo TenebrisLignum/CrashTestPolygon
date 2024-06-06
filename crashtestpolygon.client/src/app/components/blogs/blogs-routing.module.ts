ca54c38133e58f03162376327bc5c5de4e868aee
Artem Hermaniuk <artemgerman1706@gmail.com>
2024-06-06 13:29:03 +0300

    Implemented add-article component

---
+++ crashtestpolygon.client/src/app/components/blogs/blogs-routing.module.ts
@@ -0,0 +1,24 @@
+import { RouterModule, Routes } from "@angular/router";
+import { BlogsComponent } from "./blogs.component";
+import { AddArticleComponent } from "./add-article/add-article.component";
+import { NgModule } from "@angular/core";
+import { ArticlesListComponent } from "./articles-list/articles-list.component";
+import { AdminGuard } from "../../core/guards/admin.guard";
+
+const blogsRoutes: Routes = [
+    {
+        path: 'blogs',
+        component: BlogsComponent,
+        children: [
+            { path: '', component: ArticlesListComponent },
+            { path: 'add', component: AddArticleComponent, canActivate: [AdminGuard] }
+        ]
+    }
+];
+
+@NgModule({
+    imports: [RouterModule.forChild(blogsRoutes)],
+    exports: [RouterModule]
+})
+
+export class BlogsRoutingModule { }
