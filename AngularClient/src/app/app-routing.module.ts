11530584264c8b156198b4080706b0f585327bfd
Artem Hermaniuk <artemgerman1706@gmail.com>
2024-06-25 13:40:21 +0300

    Implemented lazy load for blogs module

--- AngularClient/src/app/app-routing.module.ts
+++ AngularClient/src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LandingComponent } from './components/landing/landing.component';
-import { BlogsComponent } from './components/blogs/blogs.component';
 import { LoginComponent } from './components/login/login.component';
 import { LoginGuard } from './core/guards/login.guard';
 import { Error404Component } from './core/shared/error-404/error-404.component';
@@ -11,7 +10,7 @@ import { Error401Component } from './core/shared/error-401/error-401.component';
 
 const routes: Routes = [
     { path: '', component: LandingComponent },
-    { path: 'blogs', component: BlogsComponent },
+    { path: 'blogs', loadChildren: () => import('./components/blogs/blogs.module').then(x => x.BlogsModule) },
     { path: 'login', component: LoginComponent, canActivate: [LoginGuard] },
     { path: 'unauthorized', component: Error401Component },
     { path: 'forbidden', component: Error403Component },
