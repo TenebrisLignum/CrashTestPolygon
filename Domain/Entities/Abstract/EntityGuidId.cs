6a8bbee8d3e6e67be5ce8012d3f0b412e455d8d1
Artem Hermaniuk <artemgerman1706@gmail.com>
2024-06-28 11:47:26 +0300

    Decomposed Entity to subclasses

---
+++ Domain/Entities/Abstract/EntityGuidId.cs
@@ -0,0 +1,16 @@
+﻿namespace Domain.Entities.Abstract
+{
+    public abstract class EntityGuidId: Entity
+    {
+        public virtual Guid Id { get; set; }
+
+        protected EntityGuidId()
+        {
+        }
+
+        protected EntityGuidId(Guid id)
+        {
+            Id = id;
+        }
+    }
+}
---
+++ Domain/Entities/Abstract/EntityIntId.cs
@@ -0,0 +1,16 @@
+﻿namespace Domain.Entities.Abstract
+{
+    public abstract class EntityIntId : Entity
+    {
+        public virtual int Id { get; set; }
+
+        protected EntityIntId()
+        {
+        }
+
+        protected EntityIntId(int id)
+        {
+            Id = id;
+        }
+    }
+}
