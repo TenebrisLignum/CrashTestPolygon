cc902fc3040974ac4257b2d79a1f1e464dba1e8c
Artem Hermaniuk <artemgerman1706@gmail.com>
2024-07-01 13:19:34 +0300

    Added LoggedInGuard

--- AngularClient/src/app/app-routing.module.ts
+++ AngularClient/src/app/app-routing.module.ts
@@ -9,13 +9,14 @@ import { Error404Component } from './core/shared/error-404/error-404.component';
 import { Error500Component } from './core/shared/error-500/error-500.component';
 import { FeaturesComponent } from './components/features/features.component';
 import { SignupComponent } from './components/sign-up/sign-up.component';
+import { LoggedInGuard } from './core/guards/logged-in.guard';
 
 const routes: Routes = [
     { path: '', component: LandingComponent },
     { path: 'features', component: FeaturesComponent },
     { path: 'blogs', loadChildren: () => import('./components/blogs/blogs.module').then(x => x.BlogsModule) },
-    { path: 'login', component: LoginComponent, canActivate: [LoginGuard] },
-    { path: 'signup', component: SignupComponent, canActivate: [LoginGuard] },
+    { path: 'login', component: LoginComponent, canActivate: [LoggedInGuard] },
+    { path: 'signup', component: SignupComponent, canActivate: [LoggedInGuard] },
     { path: 'unauthorized', component: Error401Component },
     { path: 'forbidden', component: Error403Component },
     { path: 'not-found', component: Error404Component },
--- AngularClient/src/app/core/guards/logged-in.guard.ts
+++ AngularClient/src/app/core/guards/logged-in.guard.ts
@@ -2,15 +2,15 @@ import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { AuthService } from '../services/auth/auth.service';
 
-export const LoginGuard: CanActivateFn = (route, state) => {
+export const LoggedInGuard: CanActivateFn = (route, state) => {
     const authService = inject(AuthService);
     const router = inject(Router);
 
-    if (authService.isLoggedIn()) {
+    if (!authService.isLoggedIn()) {
         return true;
     }
     else {
-        router.navigateByUrl('/login');
+        router.navigateByUrl('/');
         return false;
     }
 };
